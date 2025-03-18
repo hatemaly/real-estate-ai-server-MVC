7c1eeec9e6209684858847bb6b66bf8b02f66331
abdelrhman yasser2020 <57268656+abdoafage@users.noreply.github.com>
2025-03-18 22:08:36 +0000

    feat: add vector store interface

---
+++ app/models/property_models/property_chunk.py
@@ -0,0 +1,11 @@
+from pydantic import  Field
+from typing import List, Optional
+from datetime import datetime
+
+from app.models.base_model import BaseModelApp
+from app.models.property_models.price import Price
+from enum import Enum
+
+
+
+class Prooerty
